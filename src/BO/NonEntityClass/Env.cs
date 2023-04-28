6e0cf154b33bddc2e8348aca82c6ba1d16a0d231
dotnetdoktor <HSchwichtenberg@users.noreply.github.com>
2023-04-28 10:09:15 +0200

    V7.12.2 gesetzt

--- src/BO/NonEntityClass/Env.cs
+++ src/BO/NonEntityClass/Env.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BO.NonEntityClass;
 
-namespace BO.NonEntityClass;
+namespace BO;
 
 public class Env : IEnv
 {

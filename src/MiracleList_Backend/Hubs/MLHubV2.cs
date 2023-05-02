4a0f91294d08871e783e8062881b8b1f9986319b
dotnetdoktor <HSchwichtenberg@users.noreply.github.com>
2023-05-02 21:03:12 +0200

    c

--- src/MiracleList_Backend/Hubs/MLHubV2.cs
+++ src/MiracleList_Backend/Hubs/MLHubV2.cs
@@ -1,84 +1,85 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using ITVisions;
 using Microsoft.AspNetCore.SignalR;
 
-namespace MiracleList_Backend.Hubs
+namespace MiracleList_Backend.Hubs;
+
+
+/// <summary>
+/// Version 2 des Hub: Für Gruppenzugehörigkeit wird User-GUID statt Token verwendet
+/// </summary>
+public class MLHubV2 : Hub<IMLHub>
 {
+ //[HubMethodName("Register")] -> nur, wenn Name der Nachricht anders als Methodennamen sein soll
+ public async Task Register(string token)
+ {
+  var u = new BL.UserManager(token);
+  // Prüfe die Gültigkeit des Tokens
+  if (u.IsValid() != BL.UserManager.TokenValidationResult.Ok) return;
+  // Alle Clients (== Browser) eines Benutzers bilden eine Gruppe (anhand der User-GUID)
+  await base.Groups.AddToGroupAsync(Context.ConnectionId, u.CurrentUser.UserGUID.ToString());
+  CUI.PrintStep("SignalR.Register: Connection " + Context.ConnectionId + " for User" + u.CurrentUser.UserName);
+ }
+
+ public async Task CategoryListUpdate(string token)
+ {
+  var u = new BL.UserManager(token);
+  // Prüfe die Gültigkeit des Tokens
+  if (u.IsValid() != BL.UserManager.TokenValidationResult.Ok) return;
+  // Protokollierung
+  new BL.LogManager().Log(BO.Event.Call, BO.Severity.Information, "User=" + u.CurrentUser.UserName, nameof(CategoryListUpdate));
+  // Sende Benachrichtigung an die ganze Gruppe, außer der aktuellen Verbindung!
+  await Clients.OthersInGroup(u.CurrentUser.UserGUID.ToString()).CategoryListUpdate(Context.ConnectionId);
+  CUI.PrintStep("SignalR.CategoryListUpdate: Connection " + Context.ConnectionId + " for User" + u.CurrentUser.UserName);
+ }
+
+ public async Task TaskListUpdate(string token, int categoryID)
+ {
+  var u = new BL.UserManager(token);
+  // Prüfe die Gültigkeit des Tokens
+  if (u.IsValid() != BL.UserManager.TokenValidationResult.Ok) return;
+  // Protokollierung
+  new BL.LogManager().Log(BO.Event.Call, BO.Severity.Information, "User=" + u.CurrentUser.UserName + "Category=" + categoryID, nameof(TaskListUpdate));
+  // Sende Benachrichtigung an die ganze Gruppe, außer der aktuellen Verbindung!
+  await Clients.OthersInGroup(u.CurrentUser.UserGUID.ToString()).TaskListUpdate(Context.ConnectionId, categoryID);
+  CUI.PrintStep("SignalR.TaskListUpdate: Connection " + Context.ConnectionId + " for User" + u.CurrentUser.UserName);
+ }
 
  /// <summary>
- /// Version 2 des Hub: Für Gruppenzugehörigkeit wird der Username statt Token verwendet
+ /// Beim Aufbau einer Verbindung eines Clients zu diesem Hub
  /// </summary>
- public class MLHubV2 : Hub<IMLHub>
+ public override async Task OnConnectedAsync()
  {
-  //[HubMethodName("Register")] -> nur, wenn Name der Nachricht anders als Methodennamen sein soll
-  public async Task Register(string token)
-  {
-   var u = new BL.UserManager(token);
-   // Prüfe die Gültigkeit des Tokens
-   if (u.IsValid() != BL.UserManager.TokenValidationResult.Ok) return;
-   // Alle Clients (== Browser) eines Benutzers bilden eine Gruppe (anhand des username)
-   await base.Groups.AddToGroupAsync(Context.ConnectionId, u.CurrentUser.UserGUID.ToString());
-   CUI.PrintStep("SignalR.Register: Connection " + Context.ConnectionId + " for User" + u.CurrentUser.UserName);
-  }
+  var logMessage = "";
+  logMessage = logMessage.AddLine(DateTime.Now.ToString() + " OnConnectedAsync: " + Context.ConnectionId.ToString() + "/" + Context.UserIdentifier);
+  //foreach (var f in Context.Features)
+  //{
+  // logMessage = logMessage.AddLine(f.Key + ": " + f.Value);
+  //}
+  WriteToLog(logMessage);
+  await base.OnConnectedAsync();
+ }
 
-  public async Task CategoryListUpdate(string token)
-  {
-   var u = new BL.UserManager(token);
-   // Prüfe die Gültigkeit des Tokens
-   if (u.IsValid() != BL.UserManager.TokenValidationResult.Ok) return;
-   // Protokollierung
-   new BL.LogManager().Log(BO.Event.Call, BO.Severity.Information, "User=" + u.CurrentUser.UserName, nameof(CategoryListUpdate));
-   // Sende Benachrichtigung an die ganze Gruppe, außer der aktuellen Verbindung!
-   await Clients.OthersInGroup(u.CurrentUser.UserGUID.ToString()).CategoryListUpdate(Context.ConnectionId);
-   CUI.PrintStep("SignalR.CategoryListUpdate: Connection " + Context.ConnectionId + " for User" + u.CurrentUser.UserName);
-  }
+ /// <summary>
+ /// Beim Beenden einer Verbindung eines Clients zu diesem Hub
+ /// </summary>
+ public override async Task OnDisconnectedAsync(Exception exception)
+ {
+  var logMessage = "";
+  logMessage = logMessage.AddLine(DateTime.Now.ToString() + " OnDisconnectedAsync: " + Context.ConnectionId.ToString() + "/" + Context.UserIdentifier);
+  WriteToLog(logMessage);
+  await base.OnDisconnectedAsync(exception);
+ }
 
-  public async Task TaskListUpdate(string token, int categoryID)
+ static bool LogActive = false;
+ private static void WriteToLog(string logMessage)
+ {
+  if (!LogActive) return;
+  using (StreamWriter w = File.AppendText(@"log.txt"))
   {
-   var u = new BL.UserManager(token);
-   // Prüfe die Gültigkeit des Tokens
-   if (u.IsValid() != BL.UserManager.TokenValidationResult.Ok) return;
-   // Protokollierung
-   new BL.LogManager().Log(BO.Event.Call, BO.Severity.Information, "User=" + u.CurrentUser.UserName + "Category=" + categoryID, nameof(TaskListUpdate));
-   // Sende Benachrichtigung an die ganze Gruppe, außer der aktuellen Verbindung!
-   await Clients.OthersInGroup(u.CurrentUser.UserGUID.ToString()).TaskListUpdate(Context.ConnectionId, categoryID);
-   CUI.PrintStep("SignalR.TaskListUpdate: Connection " + Context.ConnectionId + " for User" + u.CurrentUser.UserName);
+   w.WriteLine(logMessage + "\n");
   }
-
-  /// <summary>
-  /// Beim Aufbau einer Verbindung eines Clients zu diesem Hub
-  /// </summary>
-  public override async Task OnConnectedAsync()
-  {
-   var logMessage = "";
-   logMessage = logMessage.AddLine(DateTime.Now.ToString() + " OnConnectedAsync: " + Context.ConnectionId.ToString() + "/" + Context.UserIdentifier);
-   //foreach (var f in Context.Features)
-   //{
-   // logMessage = logMessage.AddLine(f.Key + ": " + f.Value);
-   //}
-   WriteToLog(logMessage);
-   await base.OnConnectedAsync();
-  }
-
-  /// <summary>
-  /// Beim Beenden einer Verbindung eines Clients zu diesem Hub
-  /// </summary>
-  public override async Task OnDisconnectedAsync(Exception exception)
-  {
-   var logMessage = "";
-   logMessage = logMessage.AddLine(DateTime.Now.ToString() + " OnDisconnectedAsync: " + Context.ConnectionId.ToString() + "/" + Context.UserIdentifier);
-   WriteToLog(logMessage);
-   await base.OnDisconnectedAsync(exception);
-  }
-
-  private static void WriteToLog(string logMessage)
-  {
-   //using (StreamWriter w = File.AppendText(@"t:\log.txt"))
-   //{
-   // w.WriteLine(logMessage + "\n");
-   //}
-  }
-
  }
 }

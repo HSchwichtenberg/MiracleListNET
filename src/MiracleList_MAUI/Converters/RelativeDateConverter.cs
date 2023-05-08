dfaf84ccfe355f19f6aa311c79fa0f9d5fc880c4
André Krämer <andre.kraemer@qualitybytes.de>
2023-05-08 13:28:59 +0200

    Formatierung des Fälligkeitsdatums auf der Aufgabenseite unter .NET MAUI
    Formatierung des Fälligkeitsdatums über einen DataTrigger

---
+++ src/MiracleList_MAUI/Converters/RelativeDateConverter.cs
@@ -0,0 +1,22 @@
+﻿using ITVisions;
+using System.Globalization;
+
+namespace MiracleList_MAUI.Converters
+{
+    public class RelativeDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime date)
+            {
+                return date.Date.ToRelativeDate(true);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/MiracleList_MAUI/Converters/TaskStatusConverter.cs
@@ -0,0 +1,22 @@
+﻿using ITVisions;
+using System.Globalization;
+
+namespace MiracleList_MAUI.Converters
+{
+    public class TaskStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime date)
+            {
+                return date.Date < DateTime.Now.Date ? "overdue" : "open";
+            }
+            return "open";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

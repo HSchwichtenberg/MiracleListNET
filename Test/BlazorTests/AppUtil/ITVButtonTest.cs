7e842bd2238986255eec5eecc30d90623e97026d
dotnetdoktor <HSchwichtenberg@users.noreply.github.com>
2023-08-31 18:38:12 +0200

    Mehr Tests

---
+++ Test/BlazorTests/AppUtil/ITVButtonTest.cs
@@ -0,0 +1,132 @@
+
+using System;
+using System.Threading.Tasks;
+using AngleSharpWrappers;
+using BlazorTests.Mocks;
+using ITVisions;
+using ITVisions.Blazor;
+using ITVisions.Blazor.Controls;
+using Telerik.JustMock;
+
+namespace BlazorTests.AppUtil;
+
+/// <summary>
+/// These tests are written entirely in C#.
+/// Learn more at https://bunit.dev/docs/getting-started/writing-tests.html#creating-basic-tests-in-cs-files
+/// </summary>
+public class ITVButtonTest : TestContext
+{
+ public ITVButtonTest()
+ {
+
+  Services.AddSingleton<BlazorUtil>(new BlazorUtil(this.JSInterop.JSRuntime, new MockNavigationManager(), null));
+ }
+ [Fact]
+ public void EmptyButton()
+ {
+  // Arrange
+  var cut = RenderComponent<ITVButton>();
+
+  // Assert that content of the paragraph shows counter at zero
+  cut.Find("button").Text().MarkupMatches("");
+ }
+
+ [Fact]
+ public void ButtonWithHTMLContent()
+ {
+  string content = "Test<b>button</b>";
+  // Arrange
+  var cut = RenderComponent<ITVButton>(p => p.AddChildContent(content));
+
+  // Assert that the counter was incremented
+  cut.Find("button").Html().MarkupMatches(content);
+ }
+
+ [Fact]
+ public async Task ButtonClickWithErrorHandler()
+ {
+  int delay = 2;
+  string content = "Test<b>button</b>";
+  // Arrange
+
+  Action<MouseEventArgs> onClickHandler = (x) =>
+  {
+   throw new ApplicationException("Testfehler");
+  };
+
+  Action<Exception> onErrorHandler = (ex) =>
+  {
+   Assert.IsType<ApplicationException>(ex);
+   Assert.Equal("Testfehler", ex.Message);
+  };
+
+  var cut = RenderComponent<ITVButton>(p => p.AddChildContent(content).Add(x => x.AnimationSeconds, (byte)delay).Add(x=>x.onClick, onClickHandler).Add(x=>x.onError, onErrorHandler));
+
+  Assert.Equal(1, cut.RenderCount);
+
+  IElement b = cut.Find("button");
+  b.Html().MarkupMatches(content);
+  Assert.Null(b.GetAttribute("disabled"));
+
+  b.Click();
+
+  // Warten bis Schaltfläche deaktiviert
+  cut.WaitForState(() => b.GetAttribute("disabled").IsNotNull());
+  // Animation sollte nun aktiv sein
+  b.Html().MarkupMatches("<img src=\"/_content/ITVisions.Blazor/img/ITVButtonProgress.gif\" width=\"20\" style=\"margin-right:8px;\">" + content);
+
+
+  // Warten bis Schaltfläche wieder aktiv ist
+  cut.WaitForState(() => b.GetAttribute("disabled").IsNull(),new System.TimeSpan(0,0,delay));
+  cut.Render();
+
+  // jetzt sollte die Animation fertig sein
+  b.Html().MarkupMatches(content);
+
+ }
+
+
+ [Fact]
+ public async Task ButtonClickWithoutErrorHandler()
+ {
+  //JSInterop.Mode = JSRuntimeMode.Loose;
+  JSInterop.SetupVoid("console.error", @"BLAZOR: System.ApplicationException: Testfehler");
+  JSInterop.SetupVoid("console.info", @"BLAZOR: Alert");
+  JSInterop.SetupVoid("ShowAlert", @"System.ApplicationException: Testfehler");
+
+
+  int delay = 2;
+  string content = "Test<b>button</b>";
+  // Arrange
+
+  Action<MouseEventArgs> onClickHandler = (x) =>
+  {
+   throw new ApplicationException("Testfehler");
+  };
+
+
+  var cut = RenderComponent<ITVButton>(p => p.AddChildContent(content).Add(x => x.AnimationSeconds, (byte)delay).Add(x => x.onClick, onClickHandler));
+
+  Assert.Equal(1, cut.RenderCount);
+
+  IElement b = cut.Find("button");
+  b.Html().MarkupMatches(content);
+  Assert.Null(b.GetAttribute("disabled"));
+
+  b.Click();
+
+  // Warten bis Schaltfläche deaktiviert
+  cut.WaitForState(() => b.GetAttribute("disabled").IsNotNull(), new System.TimeSpan(0, 0, 10));
+  // Animation sollte nun aktiv sein
+  b.Html().MarkupMatches("<img src=\"/_content/ITVisions.Blazor/img/ITVButtonProgress.gif\" width=\"20\" style=\"margin-right:8px;\">" + content);
+
+
+  // Warten bis Schaltfläche wieder aktiv ist
+  cut.WaitForState(() => b.GetAttribute("disabled").IsNull(), new System.TimeSpan(0, 0, 10));
+  cut.Render();
+
+  // jetzt sollte die Animation fertig sein
+  b.Html().MarkupMatches(content);
+
+ }
+}
---
+++ Test/BlazorTests/Test von Komponenten mit Parametern/CounterMitParameternTest_CodeOnly.cs
@@ -0,0 +1,34 @@
+
+
+namespace BlazorTests.Test_von_Komponenten_mit_Parametern;
+
+
+/// <summary>
+/// These tests are written entirely in C#.
+/// Learn more at https://bunit.dev/docs/getting-started/writing-tests.html#creating-basic-tests-in-cs-files
+/// </summary>
+public class ITVButtonTest : TestContext
+{
+ [Fact]
+ public void CounterStartsAtZero()
+ {
+  // Arrange
+  var cut = RenderComponent<CounterMitParametern>();
+
+  // Assert that content of the paragraph shows counter at zero
+  cut.Find("p").MarkupMatches("<p>Current count: 0</p>");
+ }
+
+ [Fact]
+ public void ClickingButtonIncrementsCounter()
+ {
+  // Arrange
+  var cut = RenderComponent<CounterMitParametern>(p=>p.Add(x=>x.Increment,2).Add(x=>x.CurrentCount,40));
+
+  // Act - click button to increment counter
+  cut.Find("button").Click();
+
+  // Assert that the counter was incremented
+  cut.Find("p").MarkupMatches("<p>Current count: 42</p>");
+ }
+}

142bb5f9765abf7e470e9e1cdeece5a6dcbda2ec
dotnetdoktor <HSchwichtenberg@users.noreply.github.com>
2024-02-10 21:57:07 +0100

    Fix Dateiname war nicht angepasst auf .den vor einiger Zeit geänderten Assemblynamen Fix Eventnamen war nicht angepasst auf .NET 8

--- src/ITVisions.Blazor/wwwroot/ITV.BlazorUtil.lib.module.js
+++
@@ -1,6 +0,0 @@
-﻿export function beforeStart(options) {
- console.log("ITVisions.Blazor JavaScript initializers beforeStart", options);
-}
-export function afterStarted(blazor) {
- console.log("ITVisions.Blazor JavaScript initializers afterStarted", blazor);
-}

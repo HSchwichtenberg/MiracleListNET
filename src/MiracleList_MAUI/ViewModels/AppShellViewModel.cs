b532766b356804f5510ce681e31b516876bf0397
André Krämer <andre@andrekraemer.de>
2024-06-12 20:31:44 +0200

    MAUI: Export Funktion auf Mobile Geräten hinzufgefügt

--- src/MiracleList_MAUI/ViewModels/AppShellViewModel.cs
+++ src/MiracleList_MAUI/ViewModels/AppShellViewModel.cs
@@ -7,40 +7,48 @@ using System.Diagnostics;
 
 namespace MiracleList_MAUI.ViewModels
 {
-    [INotifyPropertyChanged]
-    public partial class AppShellViewModel
-    {
-        private readonly IAppState appState;
-        private readonly IMessenger messenger;
-        private readonly IBrowser browser;
+ [INotifyPropertyChanged]
+ public partial class AppShellViewModel
+ {
+  private readonly IAppState appState;
+  private readonly IMessenger messenger;
+  private readonly IBrowser browser;
 
-        [ObservableProperty]
-        private string username;
+  [ObservableProperty]
+  private string username;
 
-        [ObservableProperty]
-        private string statustext;
+  [ObservableProperty]
+  private string statustext;
 
-        public AppShellViewModel(IAppState appState, IMessenger messenger, IBrowser browser)
-        {
-            this.appState = appState;
-            this.messenger = messenger;
-            this.browser = browser;
-            Username = appState.Username;
-            Statustext = $"{System.Runtime.InteropServices.RuntimeInformation.OSDescription} | {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription} | MAUI {FileVersionInfo.GetVersionInfo(typeof(Shell).Assembly.Location).FileVersion} | Process #{System.Environment.ProcessId} {System.IO.Path.GetFileName(System.Environment.ProcessPath)} | Thread #{System.Threading.Thread.CurrentThread.ManagedThreadId} | {System.Diagnostics.Process.GetCurrentProcess().WorkingSet64 / 1024 / 1024} MB | {DateTime.Now.ToLongTimeString()} | {(appState.IsLoggedIn ? appState.Username : "Kein Benutzer")}";
-        }
+  public AppShellViewModel(IAppState appState, IMessenger messenger, IBrowser browser)
+  {
+   this.appState = appState;
+   this.messenger = messenger;
+   this.browser = browser;
+   Username = appState.Username;
+   try
+   {
+    Statustext = $"{System.Runtime.InteropServices.RuntimeInformation.OSDescription} | {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription} | MAUI {FileVersionInfo.GetVersionInfo(typeof(Shell).Assembly.Location).FileVersion} | Process #{System.Environment.ProcessId} {System.IO.Path.GetFileName(System.Environment.ProcessPath)} | Thread #{System.Threading.Thread.CurrentThread.ManagedThreadId} | {System.Diagnostics.Process.GetCurrentProcess().WorkingSet64 / 1024 / 1024} MB | {DateTime.Now.ToLongTimeString()} | {(appState.IsLoggedIn ? appState.Username : "Kein Benutzer")}";
+   }
+   catch
+   (PlatformNotSupportedException)
+   {
+    Statustext = $"{System.Runtime.InteropServices.RuntimeInformation.OSDescription} | {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription} | MAUI {FileVersionInfo.GetVersionInfo(typeof(Shell).Assembly.Location).FileVersion} | Process #{System.Environment.ProcessId} {System.IO.Path.GetFileName(System.Environment.ProcessPath)} | Thread #{System.Threading.Thread.CurrentThread.ManagedThreadId} | {DateTime.Now.ToLongTimeString()} | {(appState.IsLoggedIn ? appState.Username : "Kein Benutzer")}";
+   }
+  }
 
-        [RelayCommand]
-        private async Task Logout()
-        {
-            messenger.Send(new UserLoggedOutMessage(appState.Username));
-        }
+  [RelayCommand]
+  private async Task Logout()
+  {
+   messenger.Send(new UserLoggedOutMessage(appState.Username));
+  }
 
-        [RelayCommand]
-        private async Task NavigateToUrl(string url)
-        {
-            await browser.OpenAsync(url);
-        }
+  [RelayCommand]
+  private async Task NavigateToUrl(string url)
+  {
+   await browser.OpenAsync(url);
+  }
 
 
-    }
+ }
 }

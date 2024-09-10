2afb22fbd4b7f65d959503119d7fa932369c64fc
dotnetdoktor <HSchwichtenberg@users.noreply.github.com>
2024-09-10 18:53:02 +0200

    Refactoring der Komponentenliste

--- src/MiracleList_BS/Program.cs
+++ src/MiracleList_BS/Program.cs
@@ -21,7 +21,7 @@ public class Program
  {
   CreateHostBuilder(args).Build().Run();
  }
-
+ 
  public static IHostBuilder CreateHostBuilder(string[] args) =>
      Host.CreateDefaultBuilder(args)
          .ConfigureWebHostDefaults(webBuilder =>

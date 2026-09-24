5075e6a997ad865b3aad912f662d22973bac5498
E65 <no@email.de>
2026-09-24 10:20:17 +0200

    c

--- src/SamplesRCL/Komponenteneinbettung/Komponentenhost_ReferenceType/CounterData.cs
+++ src/SamplesRCL/Komponenteneinbettung/Komponentenhost_ReferenceType/CounterData.cs
@@ -8,19 +8,6 @@ namespace Samples.Komponenteneinbettung.Komponentenhost_ReferenceType
  /// </summary>
  public class CounterData
  {
-  int counter = 0;
-  public int Counter
-  {
-   get
-   {
-    Console.WriteLine("CounterData.Get=" + counter);
-    return counter;
-   }
-   set
-   {
-    Console.WriteLine("CounterData.Set=" + value);
-    this.counter = value;
-   }
-  }
+  public int Counter { get; set; } = 0;
  }
 }

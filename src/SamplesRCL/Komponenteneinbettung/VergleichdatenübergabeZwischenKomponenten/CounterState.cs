ce1e6c0579bf63b90b2db38fe6519eccf561326d
E65 <no@email.de>
2026-09-24 11:24:23 +0200

    fix Namespace

--- src/SamplesRCL/Komponenteneinbettung/VergleichdatenübergabeZwischenKomponenten/CounterState.cs
+++ src/SamplesRCL/Komponenteneinbettung/VergleichdatenübergabeZwischenKomponenten/CounterState.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Samples.Komponenteneinbettung.VergleichWertübergabeAnKomponenten;
+namespace Samples.Komponenteneinbettung.VergleichdatenübergabeZwischenKomponenten;
 
 public class CounterState
 {
